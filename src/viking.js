189279e94acaa69bd9142cc526955838e4511cb0
Nathalia Lafosse <lafossenathalia@gmail.com>
2021-11-03 22:03:31 -0400

    iterations 1-3

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,69 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health,
+    this.strength = strength
+  }
+  attack(){
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health -= damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health, strength)
+    this.name = name
+  }
+
+  receiveDamage(damage){
+    this.health -= damage
+    return(this.health > 0 ? `${this.name} has received ${damage} points of damage` : `${this.name} has died in act of combat`)
+    // if (this.health > 0) {
+    //   return `${this.name} has received ${damage} points of damage`
+    // } else {
+    //   return `${this.name} has died in act of combat`
+    // }
+  }
+
+  battleCry(){
+    return `Odin Owns You All!`
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  receiveDamage(damage){
+    this.health -= damage
+    return ((this.health > 0) ? `A Saxon has received ${damage} points of damage` : `A Saxon has died in combat`);
+    // if (this.health > 0) {
+    //   return `A Saxon has received ${damage} points of damage`
+    // } else {
+    //   return `A Saxon has died in combat`
+    // }
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = []
+  saxonArmy = []
+
+  addViking(viking){
+    this.vikingArmy.push(viking)
+  }
+  addSaxon(saxon){
+    this.saxonArmy.push(saxon)
+  }
+  vikingAttack(){
+    this.saxonArmy.receiveDamage = Viking.strength
+  }
+  saxonAttack(){}
+  showStatus(){}
+}
 
 
 
